2210d497417eb15573ded00631ab846e362d8ad9
udays3721 <udays4832@yahoo.com>
2023-05-08 10:23:53 -0700

    error

--- popup.js
+++ popup.js
@@ -20,7 +20,7 @@ chrome.contextMenus.create({
 //this will send the text to open ai if user asks a question about selected text
 chrome.contextMenus.onclicked.addlistener(function askText(info)
     {
-        if(info.menuItemId==="uniqueId1"&& info.selectionText){
+       
         const prompt=info.selectionText;
         const apiUrl = 'https://api.openai.com/v1/engines/' + model + '/completions';
 
@@ -42,37 +42,6 @@ chrome.contextMenus.onclicked.addlistener(function askText(info)
             win.document.write(`<html><body><p>${generatedText}</p></body></html>`)
         })
         .catch(error=>console.error(error));
-    }}
+    }
 )
-
-//this will send the text to a open ai for explaining the text
-chrome.contextMenus.onclicked.addlistener(function askText(info)
-{
-    if(info.menuItemId==="uniqueId2"&& info.selectionText){
-    {
-        const prompt=info.selectionText;
-        const apiUrl = 'https://api.openai.com/v1/engines/' + model + '/completions';
-
-        fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + apiKey
-            },
-        body: JSON.stringify(
-        {prompt: prompt, max_tokens: maxTokens})
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            const generatedText = data.choices[0].text;
-            console.log(generatedText);
-            const win=window.open('','_blank',"width=500,height=400");
-            win.document.write(`<html><body><p>${generatedText}</p></body></html>`)
-        })
-        .catch(error=>console.error(error));
-    }}})
-
-
-
 
